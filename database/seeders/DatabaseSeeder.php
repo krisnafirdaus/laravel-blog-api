df25d43df258e0b24b89771ddf59059ee7bf94ce
Krisna Firdaus <krisnafirdaus@gmail.com>
2026-03-28 10:27:58 +0700

    feat: db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // 5 kategori
+       $categories = Category::factory(5)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       // 20 post
+       foreach ($categories as $category) {
+           Post::factory(4)->create([
+            'category_id' => $category->id
+           ]);
+       }
     }
 }
