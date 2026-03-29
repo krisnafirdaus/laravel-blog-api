1a3af25131181b4d6978fbc3172d93146fb6848b
Krisna Firdaus <krisnafirdaus@gmail.com>
2026-03-29 09:56:24 +0700

    feat: update request

---
+++ app/Http/Requests/UpdatePostRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class UpdatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category_id' => 'sometimes|exists:categories,id',
+            'title' => 'sometimes|string|min:5|max:255',
+            'slug' => 'nullable|string|unique:posts,slug,' . $this->route('post')->id,
+            'content' => 'sometimes|string|min:50',
+            'status' => 'sometimes|in:draft,published',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'category_id.exists' => 'Kategori tidak ditemukan',
+            'title.min' => 'Judul minimal 5 karakter',
+            'content.min' => 'Konten minimal 50 karakter',
+        ];
+    }
+
+        protected function failedValidation(Validator $validator)
+        {
+            throw new HttpResponseException(response()->json([
+                'status' => false,
+                'message' => 'Validasi gagal',
+                'errors' => $validator->errors(),
+            ], 422));
+        }
+}
